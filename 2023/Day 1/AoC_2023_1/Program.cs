2790452ce9ae8b4c9586f436eabe70c48102f75d
Jxliius <julius.reiter001@gmail.com>
2023-12-02 13:18:02 +0100

    Finished AoC Day 1

--- 2023/Day 1/AoC_2023_1/Program.cs
+++ 2023/Day 1/AoC_2023_1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 
 namespace AoC_2023_1
 {
@@ -9,18 +10,52 @@ namespace AoC_2023_1
     {
         static string ReplaceNumberString(string input)
         {
-            return null;
+            string[] stringArray = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+            string[] numberArray = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+            Dictionary<string, string> stringToNumberMap = new Dictionary<string, string>();
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                stringToNumberMap[stringArray[i]] = (i + 1).ToString();
+            }
+
+            string smallstring = "";
+            string sol = "";
+            string checker = "";
+            for (int i = 0; i < input.Length; i++)
+            {
+                smallstring += input[i];
+                checker = smallstring;
+                foreach (var value in stringToNumberMap)
+                {
+                    checker = smallstring.Replace(value.Key, value.Value);
+                    if (numberArray.Any(checker.Contains))
+                    {
+                        sol += checker;
+                        checker = "";
+                        if (char.IsDigit(smallstring[smallstring.Length - 1]))
+                        {
+                            smallstring = "";
+                        }
+                        else
+                        {
+                            smallstring = Convert.ToString(input[i]);
+                        }
+                    }
+
+                }
+            }
+            return sol;
         }
 
         static void Main(string[] args)
         {
+            List<string> AllNumbersTest = new List<string>();
             string path = Path.GetFullPath(Path.Combine(System.AppContext.BaseDirectory, @"..\..\..\"));
             path = path + "input.txt";
-            //string[] numbers = System.IO.File.ReadAllLines(path);
-            string[] numbers = new string[] { "two1nine", "eightwothree", "abcone2threexyz", "xtwone3four", "4nineeightseven2", "zoneight234", "7pqrstsixteen" };
+            string[] numbers = System.IO.File.ReadAllLines(path);
+            //string[] numbers = new string[] { "two1nine", "eightwothree", "abcone2threexyz", "xtwone3four", "4nineeightseven2", "zoneight234", "7pqrstsixteen" };
             //string[] numbers = new string[4] { "1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet" };
             int sol = 0;
-            //List<string> AllValues = new List<string>();
             for (int i = 0; i < numbers.Length; i++)
             {
                 List<string> NumberList = new List<string>();
